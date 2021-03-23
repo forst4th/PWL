8fc4757e6ea14f2a3ec321f971b3acf0a017a49b
forst4th <79978622+forst4th@users.noreply.github.com>
2021-03-23 23:28:10 +0700

    Update Folder

---
+++ minggu_3/praktikum/combobox.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Club Sepak Bola Favorit ~ Inputan Combobox</title>
+</head>
+<body>
+
+    <FORM ACTION="prosescombobox.php" METHOD="POST" NAME="input">
+        <h2>Pilih Club Sepak Bola Favorit Anda :</h2>
+            <select name="club bola">
+            <option value="Arsenal">Arsenal</option>
+            <option value="Barcelona">Barcelona</option>
+            <option value="Chelsea">Chelsea</option>
+            <option value="Liverpool">Liverpool</option>
+            </select>
+        <input type="submit" name="Pilih" value="Pilih">
+    </FORM>
+</body>
+</html>
---
+++ minggu_3/praktikum/prosescombobox.php
@@ -0,0 +1,8 @@
+<?php
+
+if (isset($_POST['Pilih'])) {
+    $club_bola = $_POST['club_bola'];
+    echo "Club Sepak Bola Favorit Anda Adalah :<font color=blue><b>$club_bola</b></font>";
+    }
+
+?>
